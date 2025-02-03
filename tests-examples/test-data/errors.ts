9fc8a40e09d2b89051b97dfb719d2d370be5060f
Iuliia-10 <golynska.iuliia@gmail.com>
2025-02-03 22:02:26 +0200

    errors.ts

---
+++ tests-examples/test-data/errors.ts
@@ -0,0 +1,16 @@
+export const SIGNUP_EMPTY_FIRST_NAME = 'Name required';
+export const SIGNUP_INVALID_FIRST_NAME = 'Name is invalid';
+export const SIGNUP_VALIDATION_FIRST_NAME = 'Name has to be from 2 to 20 characters long';
+
+export const SIGNUP_EMPTY_LAST_NAME = 'Last name required';
+export const SIGNUP_INVALID_LAST_NAME = 'Last name is invalid';
+export const SIGNUP_VALIDATION_LAST_NAME = 'Last name has to be from 2 to 20 characters long';
+
+export const SIGNUP_EMPTY_EMAIL = 'Email required';
+export const SIGNUP_INVALID_EMAIL = 'Email is incorrect';
+
+export const SIGNUP_EMPTY_PASSWORD = 'Password required';
+export const SIGNUP_VALIDATION_PASSWORD = 'Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter';
+
+export const SIGNUP_EMPTY_RE_ENTER_PASSWORD = 'Re-enter password required';
+export const SIGNUP_VALIDATION_RE_ENTER_PASSWORD = 'Passwords do not match';

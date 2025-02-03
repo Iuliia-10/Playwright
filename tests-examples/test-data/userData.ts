cd74e685c9c62e6bcccad8bc9eb7d859567ee7c0
Iuliia-10 <golynska.iuliia@gmail.com>
2025-02-03 22:03:06 +0200

    userData.ts

---
+++ tests-examples/test-data/userData.ts
@@ -0,0 +1,11 @@
+export const credentials = {
+    userOne: {
+        firstName: 'Yuliia', 
+        lastName: 'Golynska',
+        email: 'golynska.iuliia+2006@gmail.com',
+        password:'1234567Bb',
+        reEnterPassword: '1234567Bb',
+    }
+
+}
+
